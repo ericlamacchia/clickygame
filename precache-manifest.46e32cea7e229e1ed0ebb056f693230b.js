b9aac05f90b4734bedd13fd3f3f134a9e4405cb2
Eric LaMacchia <ericlamacchia1@icloud.com>
2019-10-30 18:04:11 -0400

    Updates

---
+++ precache-manifest.46e32cea7e229e1ed0ebb056f693230b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ae6d9904a2e2cafce61b1fbde8cdc63",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "fcd11b72af29bb4993c8",
+    "url": "/clickygame/static/css/main.90d3a7f9.chunk.css"
+  },
+  {
+    "revision": "9121b43ffad7197e7e80",
+    "url": "/clickygame/static/js/2.5a1ee2de.chunk.js"
+  },
+  {
+    "revision": "fcd11b72af29bb4993c8",
+    "url": "/clickygame/static/js/main.6192a3c7.chunk.js"
+  },
+  {
+    "revision": "4b559b4fd354d80d88d6",
+    "url": "/clickygame/static/js/runtime-main.f06bebbb.js"
+  }
+]);
